28517f24226173acc4686dcd65e3abf20450969f
Doaa <Duaa_ali@outlook.sa>
2021-10-10 15:33:47 +0300

    third

--- app/src/main/java/com/example/postrequest/MainActivity.kt
+++ app/src/main/java/com/example/postrequest/MainActivity.kt
@@ -7,18 +7,23 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    val words = arrayListOf<String>()
+    lateinit var myRv : RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView = findViewById<TextView>(R.id.textView)
+        //val textView = findViewById<TextView>(R.id.textView)
         val addNew = findViewById<Button>(R.id.button2)
         val apiInterface = APIClient().getClient()?.create(APIInterface::class.java)
+        myRv = findViewById(R.id.recyclerView)
 
         //progress
         val progressDialog = ProgressDialog(this)
@@ -36,20 +41,24 @@ class MainActivity : AppCompatActivity() {
                 progressDialog.dismiss()
                 val resource: List<Users.UserDetails>? = response.body()
                 var userData:String? = "";
-                for(User in resource!!){
-                    userData = userData +User.name+ "\n"+User.location + "\n"+"\n"
+                for(User in resource!!) {
+                    userData = userData + User.name + "\n" + User.location + "\n" + "\n"
                 }
-                textView.text= userData
+                words.add(userData.toString())
+                rv()
             }
             override fun onFailure(call: Call<List<Users.UserDetails>>, t: Throwable) {
                 progressDialog.dismiss()
                 Toast.makeText(applicationContext, ""+t.message, Toast.LENGTH_SHORT).show();
             }
         })
-
         addNew.setOnClickListener {
                 intent = Intent(applicationContext, MainActivity2::class.java)
                 startActivity(intent)
         }
         }
+    fun rv(){
+        myRv.adapter = RecyclerViewAdapter(words)
+        myRv.layoutManager = LinearLayoutManager(this)
+    }
     }
